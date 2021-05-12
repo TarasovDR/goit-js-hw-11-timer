f0d06ac5d834c9415353f9791d4e0b90e0e934f0
TarasovDR <d.r.tarasov03@gmail.com>
2021-05-12 10:14:54 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import './styles.css';
+
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ onTick, targetDate }) {
+    this.intervalId = null;
+    this.onTick = onTick;
+    this.targetDate = targetDate;
+    this.startTimer();
+    this.stopTimer();
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  startTimer() {
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+      const time = this.getTimeComponents(deltaTime);
+      this.onTick(time);
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.intervalId);
+    const time = this.getTimeComponents(0);
+    updateClockFace(time);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  // targetDate: new Date('Aug 31, 2021'),
+  targetDate: new Date('May 12, 2021 10:09:20'),
+  onTick: updateClockFace,
+});
+
+function updateClockFace({
+  days = '00',
+  hours = '00',
+  mins = '00',
+  secs = '00',
+}) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
+if (CountdownTimer.targetDate < Date.now()) {
+  console.log(CountdownTimer.targetDate);
+  console.log(Date.now());
+  timer.init(0);
+  timer.stopTimer(0);
+} else {
+  timer.startTimer();
+}
